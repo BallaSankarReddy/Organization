f685cf584653857b485eaaf1b66e5cce487147b9
Balla.SankarReddy <sankara.scb@gmail.com>
2021-09-22 18:25:31 +0530

    removed space

--- src/main/java/com/organization/OrganizationApplication.java
+++ src/main/java/com/organization/OrganizationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrganizationApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizationApplication.class, args);
