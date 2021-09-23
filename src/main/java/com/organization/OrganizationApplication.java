08f1b31a2cf48c6fd6f027af2aca0526b52e90bc
Balla.SankarReddy <sankara.scb@gmail.com>
2021-09-23 15:49:57 +0530

    new space added

--- src/main/java/com/organization/OrganizationApplication.java
+++ src/main/java/com/organization/OrganizationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrganizationApplication {
 	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizationApplication.class, args);
